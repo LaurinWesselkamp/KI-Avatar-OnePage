948ae9b84f0bf4a4d80b109de83b62240228cedd
LaurinWesselkamp <l.wesselkamp@icloud.com>
2022-06-04 19:04:47 +0200

    Add KI

--- Assets/Ausgabe Fenster/SetNameOnScreen.cs
+++ Assets/Ausgabe Fenster/SetNameOnScreen.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using TMPro;
 
 /*
- * Dies ist eine selbst erstellte Klasse um den Text aus dem InputField in die Tabelle zu importieren
+ * Dies ist eine selbst erstellte Klasse um den Text aus dem InputField in die Tabelle zu importieren.
  * 
  * @author Laurin Wesselkamp
  * @version 01.06.2022
@@ -14,10 +14,19 @@ public class SetNameOnScreen : MonoBehaviour
 {
     public TextMeshProUGUI _kommentar;
     public TMP_InputField _kommentarInputField;
+    private bool _firstSet = false; 
 
     public void setKommentar()
     {
+        if(_firstSet == true)
+        {
+            _kommentar.text = _kommentar.text + "\n------------------------------------------------------------------------" + _kommentarInputField.text;
+            _kommentarInputField.text = "";
+            return;
+        }
+    
         _kommentar.text = _kommentarInputField.text;
+        _kommentarInputField.text = "";
+        _firstSet = true;
     }
-  
 }
